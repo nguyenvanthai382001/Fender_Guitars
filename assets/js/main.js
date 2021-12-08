8180edb629f58e7f699ef13b239aab7ffaf5b794
Thai <thainax101@gmail.com>
2021-12-08 17:04:53 +0700

    dá

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,16 +5,47 @@ AOS.init();
 
 $(window).scroll(function() {
 
-    if ($(window).scrollTop() >40) {
+    if ($(window).scrollTop() > 40) {
         $('.navigation').addClass('sticky');
     } else {
         $('.navigation').removeClass('sticky');
     }
 });
 
+//sticky search
+
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 40) {
+        $('.search-form').addClass('sticky');
+    } else {
+        $('.search-form').removeClass('sticky');
+    }
+});
+// menubar toggle
 var menutoggle = document.querySelector('.toggle');
 var navtoggle = document.querySelector('.navigation');
     menutoggle.onclick = function(){
         menutoggle.classList.toggle('active');
         navtoggle.classList.toggle('active');
     }
+
+//  search toggle c1
+
+var searchtoggle_open = document.querySelector('.open-search');
+var searchtoggle_close = document.querySelector('.close-search');
+searchtoggle_open.onclick = function(){
+    $('.search-form').addClass('open-search-a');
+}
+searchtoggle_close.onclick = function(){
+    $('.search-form').removeClass('open-search-a');
+}
+
+// c2
+
+// $(".close-search").click(function(){
+//     $(".search-form").hide();
+// });
+// $(".open-search").click(function(){
+//     $(".search-form").show();
+// });
--- assets/js/slick.js
+++ assets/js/slick.js
@@ -3,10 +3,9 @@
         speed: 300,
         slidesToShow: 1,
         slidesToScroll :1,
-        speed: 500,
         dots: true,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
+        autoplay: true,
+        autoplaySpeed: 2000,
         prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-chevron-left' aria-hidden='true'></i></button>",
         nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-chevron-right' aria-hidden='true'></i></button>"
     });
